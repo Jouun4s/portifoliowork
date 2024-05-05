02ebe4620e3dae8e79917455b549da2badfd21bd
jooj <jounas@educere>
2024-05-05 11:46:37 -0300

    estilização Page-Principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PagePrincipalComponent } from './page-principal/page-principal.component';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+    redirectTo: "/principal",
+    pathMatch: "full"
+    },
+    {
+        path: "principal",
+        component: PagePrincipalComponent,
+    }
+];
---
+++ src/app/page-principal/page-principal.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TextoComponent } from './texto/texto.component';
+import { ImagensComponent } from './imagens/imagens.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-page-principal',
+  standalone: true,
+  imports: [NavbarComponent, TextoComponent, ImagensComponent, FooterComponent],
+  templateUrl: './page-principal.component.html',
+  styleUrl: './page-principal.component.scss'
+})
+export class PagePrincipalComponent {
+
+}
